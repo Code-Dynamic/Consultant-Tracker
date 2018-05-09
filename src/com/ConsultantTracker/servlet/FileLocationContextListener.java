fa4744e2d9234f35fa3072a4621268d2b7de037f
Stephen Munro <37016649+Stephen-Munro@users.noreply.github.com>
2018-05-09 20:06:43 +0200

    adding file upload servlets

---
+++ src/com/ConsultantTracker/servlet/FileLocationContextListener.java
@@ -0,0 +1,54 @@
+package com.ConsultantTracker.servlet;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FileLocationContextListener
+ */
+public class FileLocationContextListener implements ServletContextListener{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+    	String rootPath = System.getProperty("catalina.home");
+    	ServletContext ctx = servletContextEvent.getServletContext();
+    	String relativePath = ctx.getInitParameter("tempfile.dir");
+    	File file = new File(rootPath + File.separator + relativePath);
+    	if(!file.exists()) file.mkdirs();
+    	System.out.println("File Directory created to be used for storing files");
+    	ctx.setAttribute("FILES_DIR_FILE", file);
+    	ctx.setAttribute("FILES_DIR", "C:\\Users\\steph\\Desktop\\");
+    }
+    public FileLocationContextListener() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
