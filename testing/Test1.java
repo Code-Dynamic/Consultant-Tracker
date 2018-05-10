074d08e3c3fc512a43660fcc288ed38ad88ef43c
Hulisani Mudimeli <hulisanimudimeli@gmail.com>
2018-05-10 19:30:12 +0200

    Testing Http Requests

--- testing/Test1.java
+++ testing/Test1.java
@@ -2,12 +2,40 @@ import java.util.concurrent.TimeUnit;
 
 public class Test1{
 	public static void main(String[] args) {
-		System.out.println("==> Creating Dummy Project...");
+		/*System.out.println("==> Creating Dummy Project...");
 		try{
 			TimeUnit.SECONDS.sleep(1);
 		}catch(InterruptedException e){
 			System.out.println("...");
 		}
-		System.out.println("==> Dummy Project Created...");		
+		System.out.println("==> Dummy Project Created...");	*/
+		HttpURLConnectionExample http = new HttpURLConnectionExample();
+		String url = "http://localhost:8080/Consultant-Tracker/emplist.svc/Clients?$format=json";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
 	}
 }
