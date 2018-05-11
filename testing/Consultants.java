9365e2c9c876d075ccb840656b6e58d0c60f26ed
Hulisani Mudimeli <hulisanimudimeli@gmail.com>
2018-05-11 02:25:24 +0200

    Jenkins Testing Files Added

---
+++ testing/Consultants.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class Consultants{
+	
+	public static void main(String[] args) throws Exception {
+		/*System.out.println("==> Creating Dummy Consultants...");
+		try{
+			TimeUnit.SECONDS.sleep(1);
+		}catch(InterruptedException e){
+			System.out.println("...");
+		}
+		System.out.println("==> Dummy Consultants Created...");	*/
+		String USER_AGENT = "Mozilla/5.0";
+		Consultants http = new Consultants();
+		String url = "http://localhost:8080/Consultant-Tracker/emplist.svc/Consultants?$format=json";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+	
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+	}
+}
---
+++ testing/Projects.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class Project{
+	
+	public static void main(String[] args) throws Exception {
+		/*System.out.println("==> Creating Dummy Project...");
+		try{
+			TimeUnit.SECONDS.sleep(1);
+		}catch(InterruptedException e){
+			System.out.println("...");
+		}
+		System.out.println("==> Dummy Project Created...");	*/
+		String USER_AGENT = "Mozilla/5.0";
+		Project http = new Project();
+		String url = "http://localhost:8080/Consultant-Tracker/emplist.svc/Projects?$format=json";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+	
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+	}
+}
