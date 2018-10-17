44b4727a10415599d99ab5d8234c9e6e4697de73
NgoniMujuru <u16285256@tuks.co.za>
2018-10-17 17:14:17 +0200

    Added MarkProjectCompleted Servlet

---
+++ src/com/ConsultantTracker/servlet/MarkProjectCompleted.java
@@ -0,0 +1,61 @@
+package com.ConsultantTracker.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ConsultantTracker.model.Project;
+
+/**
+ * Servlet implementation class MarkProjectCompleted
+ */
+public class MarkProjectCompleted extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MarkProjectCompleted() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String proj_ID = request.getParameter("projectID");	//set project ID from request
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPATest");
+		EntityManager em = emf.createEntityManager();
+		Project p = em.find(Project.class, Integer.parseInt(proj_ID));
+		p.setProject_Completed(true);
+		//saving the data to the database
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
